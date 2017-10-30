ad391c289b6309cb5a10d957e725ba7b979cacb0
Sergi Miral <sergimiral@gmail.com>
2017-10-30 02:32:42 +0100

    Added ctrl+tab and ctrl+shift+tab shorcuts
    Managed to listen for the corresponding shortcuts and navigating
    between tabs works, but the implemention could defnitely improve.
    Also it would be great to check about optimising for accessibility,
    which could also simplify the way of knowing the next & prev tabs.

--- resources/debugger/components/app.jsx
+++ resources/debugger/components/app.jsx
@@ -1,10 +1,11 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { withRouter } from 'react-router'
-import { NavLink } from 'react-router-dom'
-import { css } from 'emotion'
-import styled from 'react-emotion'
-import bridgeHandler from '../handler'
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import { connect } from "react-redux";
+import { withRouter } from "react-router";
+import { NavLink } from "react-router-dom";
+import { css } from "emotion";
+import styled from "react-emotion";
+import bridgeHandler from "../handler";
 
 const Container = styled.div`
   position: fixed;
@@ -12,17 +13,17 @@ const Container = styled.div`
   height: 100%;
   overflow: hidden;
   display: flex;
-`
+`;
 
 const TabBar = styled.div`
-  position: fixed ;
+  position: fixed;
   z-index: 1;
   ul {
     /* margin-left: 20px; */ /* bring back this if titlebar is hidden */
     display: flex;
     list-style: none;
   }
-`
+`;
 
 const Tab = styled(NavLink)`
   height: 30px;
@@ -50,7 +51,7 @@ const Tab = styled(NavLink)`
     opacity: 0.5;
     text-decoration: line-through;
   }
-`
+`;
 
 const TabContent = styled.div`
   flex: 1;
@@ -58,24 +59,74 @@ const TabContent = styled.div`
   overflow: auto;
   display: flex;
   flex-direction: row;
-`
+`;
 
 const Label = styled.span`
   font-size: 13px;
   text-transform: capitalize;
   font-weight: normal;
   padding-top: 3px;
-`
+`;
 
 const selectedTab = css`
   opacity: 1 !important;
   border-bottom: 1px solid #3d85ee !important;
-`
+`;
 
 class App extends Component {
   constructor(props) {
-    super(props)
-    bridgeHandler(props.dispatch)
+    super(props);
+    bridgeHandler(props.dispatch);
+  }
+
+  componentDidMount() {
+    this.listenForShortcuts();
+  }
+
+  navigateToTab(direction) {
+    var nextTab;
+    var prevTab;
+    var tabs = ReactDOM.findDOMNode(this).getElementsByClassName("tab");
+    for (var i = 0; i < tabs.length; i++) {
+      if (tabs[i].getAttribute("aria-current") == "true") {
+        if (i == 0) {
+          nextTab = i + 1;
+          prevTab = tabs.length - 1;
+        } else if (i == tabs.length - 1) {
+          nextTab = 0;
+          prevTab = i - 1;
+        } else {
+          nextTab = i + 1;
+          prevTab = i - 1;
+        }
+        break;
+      }
+    }
+    if (direction == "next") {
+      tabs[nextTab].click();
+    }
+    if (direction == "prev") {
+      tabs[prevTab].click();
+    }
+  }
+
+  listenForShortcuts() {
+    var self = this;
+    document.addEventListener("keydown", function(event) {
+      if (
+        event.key == "Tab" &&
+        event.ctrlKey == true &&
+        event.shiftKey == false
+      ) {
+        self.navigateToTab("next");
+      } else if (
+        event.key == "Tab" &&
+        event.ctrlKey == true &&
+        event.shiftKey == true
+      ) {
+        self.navigateToTab("prev");
+      }
+    });
   }
 
   render() {
@@ -84,25 +135,25 @@ class App extends Component {
         <TabBar>
           <ul>
             <li>
-              <Tab to="/console" activeClassName={selectedTab}>
+              <Tab className="tab" to="/console" activeClassName={selectedTab}>
                 {/* <span>🗄</span> */}
                 <Label>Console</Label>
               </Tab>
             </li>
             <li>
-              <Tab to="/elements" activeClassName={selectedTab}>
+              <Tab className="tab" to="/elements" activeClassName={selectedTab}>
                 {/* <span>💎</span> */}
                 <Label>State</Label>
               </Tab>
             </li>
             <li>
-              <Tab to="/network" activeClassName={selectedTab}>
+              <Tab className="tab" to="/network" activeClassName={selectedTab}>
                 {/* <span>🌐</span> */}
                 <Label>Network</Label>
               </Tab>
             </li>
             <li>
-              <Tab to="/actions" activeClassName={selectedTab}>
+              <Tab className="tab" to="/actions" activeClassName={selectedTab}>
                 {/* <span>🛎</span> */}
                 <Label>Actions</Label>
               </Tab>
@@ -111,8 +162,8 @@ class App extends Component {
         </TabBar>
         <TabContent>{this.props.children}</TabContent>
       </Container>
-    )
+    );
   }
 }
 
-export default withRouter(connect()(App))
+export default withRouter(connect()(App));
