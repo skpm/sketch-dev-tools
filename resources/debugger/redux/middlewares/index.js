46e963a5ac211128db7ff655308b9e9f7e91383f
mathieudutour <mathieu@dutour.me>
2017-10-30 11:34:35 +0100

    add redux-logger

--- resources/debugger/redux/middlewares/index.js
+++ resources/debugger/redux/middlewares/index.js
@@ -1,3 +1,16 @@
 import sketchPlugin from './sketch-plugin'
 
-export default sketchPlugin
+const middlewares = [sketchPlugin]
+
+if (process.env.NODE_ENV !== 'production') {
+  const { createLogger } = require('redux-logger') // eslint-disable-line
+  const logger = createLogger({
+    collapsed: true,
+    duration: true,
+    timestamp: false,
+    predicate: (getState, action) => action.type !== 'REDUX_STORAGE_SAVE',
+  })
+  middlewares.push(logger)
+}
+
+export default middlewares
--- resources/debugger/redux/store.js
+++ resources/debugger/redux/store.js
@@ -2,6 +2,6 @@ import { createStore, applyMiddleware } from 'redux'
 import rootReducer from './ducks'
 import middlewares from './middlewares'
 
-const store = applyMiddleware(middlewares)(createStore)(rootReducer)
+const store = applyMiddleware(...middlewares)(createStore)(rootReducer)
 
 export default store
