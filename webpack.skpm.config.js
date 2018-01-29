68660397180a4b97b1b46913ed76bb3b8a7c72c7
Roman Pavlov <rspaulau@gmail.com>
2018-01-29 18:21:50 +0300

    configure webpack to transpile 'cocoascript-class' package to es5. allows running sketch-dev-tools on an older mac. this might gonna be reverted

--- webpack.skpm.config.js
+++ webpack.skpm.config.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-not-accumulator-reassign/no-not-accumulator-reassign */
 const webpack = require('webpack')
+const path = require('path')
 
 module.exports = config => {
   config.resolve.extensions = ['.sketch.js', '.js', '.jsx']
@@ -28,6 +29,17 @@ module.exports = config => {
         loader: 'css-loader',
       },
     ],
+  })
+  config.module.rules.push({
+    test: /\.jsx?$/,
+    include: [path.resolve(__dirname, 'node_modules/cocoascript-class')],
+    use: {
+      loader: 'babel-loader',
+      options: {
+        babelrc: false,
+        presets: ['babel-preset-airbnb'],
+      },
+    },
   })
 
   config.plugins.push(
