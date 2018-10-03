73bdecc8dfabb33e16abfa0a0dbefd540fc5bfcb
mathieudutour <mathieu@dutour.me>
2018-10-03 18:05:57 +0200

    fix loggin nsexception without userinfo

--- src/catch-all-actions.js
+++ src/catch-all-actions.js
@@ -11,6 +11,7 @@ export function onAction(context) {
   if (!isDebuggerPresent()) {
     return undefined
   }
+  const { action, actionContext } = context
   const options = {
     withAncestors: Settings.settingForKey('withAncestors') || false,
     sourcemaps:
@@ -18,25 +19,23 @@ export function onAction(context) {
         ? Settings.settingForKey('sourcemaps')
         : true,
   }
-  const name = String(context.action)
+  const name = String(action)
   if (name === LOG_ACTION) {
     const ourCommand = String(__command.identifier())
-    const commandTriggeringTheLog = String(
-      context.actionContext.command.identifier()
-    )
+    const commandTriggeringTheLog = String(actionContext.command.identifier())
     if (commandTriggeringTheLog !== ourCommand) {
       const values = []
-      const nativeValues = context.actionContext.payload
+      const nativeValues = actionContext.payload
       for (let i = 0; i < nativeValues.length; i += 1) {
         values.push(prepareValue(nativeValues[i], options))
       }
       const payload = {
         ts: Date.now(),
-        type: String(context.actionContext.level),
+        type: String(actionContext.level),
         values,
         stack: [
           {
-            file: String(context.actionContext.command.identifier()),
+            file: commandTriggeringTheLog,
           },
         ],
       }
@@ -46,6 +45,6 @@ export function onAction(context) {
   }
   sendToDebugger(ADD_ACTION, {
     name,
-    context: prepareValue(context.actionContext, options),
+    context: prepareValue(actionContext, options),
   })
 }
