9835ce5f2bb98fcc22014b9c157533a719ba189a
mathieudutour <mathieu@dutour.me>
2019-03-05 17:05:49 -0400

    separate handler for logs

--- src/catch-all-actions.js
+++ src/catch-all-actions.js
@@ -1,17 +1,17 @@
 /* globals __command */
-import Settings from 'sketch/settings' // eslint-disable-line
+import Settings from 'sketch/settings'
 import { prepareValue } from 'sketch-utils'
 import { isDebuggerPresent, sendToDebugger } from '../debugger'
 import { ADD_ACTION, ADD_LOG } from '../shared-actions'
 
 const LOG_ACTION = 'Log.finish'
 
-// eslint-disable-next-line
-export function onAction(context) {
+export function onLogFinish(context) {
   if (!isDebuggerPresent()) {
-    return undefined
+    return
   }
-  const { action, actionContext } = context
+
+  const { actionContext } = context
   const options = {
     withAncestors: Settings.settingForKey('withAncestors') || false,
     sourcemaps:
@@ -19,32 +19,71 @@ export function onAction(context) {
         ? Settings.settingForKey('sourcemaps')
         : true,
   }
-  const name = String(action)
-  if (name === LOG_ACTION) {
-    const ourCommand = String(__command.identifier())
-    const commandTriggeringTheLog = String(actionContext.command.identifier())
-    if (commandTriggeringTheLog !== ourCommand) {
-      const values = []
-      const nativeValues = actionContext.payload
-      for (let i = 0; i < nativeValues.length; i += 1) {
-        values.push(prepareValue(nativeValues[i], options))
-      }
-      const payload = {
-        ts: Date.now(),
-        type: String(actionContext.level),
-        values,
-        stack: [
-          {
-            file: commandTriggeringTheLog,
-          },
-        ],
-      }
 
-      sendToDebugger(ADD_LOG, payload)
+  const ourCommand = String(__command.identifier())
+  const commandTriggeringTheLog = String(actionContext.command.identifier())
+
+  let value
+
+  try {
+    value = prepareValue(actionContext, options)
+  } catch (err) {
+    value = {
+      value: {
+        payload: {
+          value: [actionContext.stringValue],
+        },
+      },
     }
   }
+
+  if (commandTriggeringTheLog !== ourCommand) {
+    const payload = {
+      ts: Date.now(),
+      type: String(actionContext.level),
+      values: value.value.payload.value,
+      stack: [
+        {
+          file: commandTriggeringTheLog,
+        },
+      ],
+    }
+
+    sendToDebugger(ADD_LOG, payload)
+  }
+
   sendToDebugger(ADD_ACTION, {
-    name,
-    context: prepareValue(actionContext, options),
+    name: 'Log.finish',
+    context: value,
   })
+}
+
+// eslint-disable-next-line
+export function onAction(context) {
+  if (!isDebuggerPresent()) {
+    return
+  }
+  const { action, actionContext } = context
+  const name = String(action)
+
+  if (name === LOG_ACTION) {
+    // handled by the other action handler
+    return
+  }
+
+  const options = {
+    withAncestors: Settings.settingForKey('withAncestors') || false,
+    sourcemaps:
+      typeof Settings.settingForKey('sourcemaps') !== 'undefined'
+        ? Settings.settingForKey('sourcemaps')
+        : true,
+  }
+  try {
+    sendToDebugger(ADD_ACTION, {
+      name,
+      context: prepareValue(actionContext, options),
+    })
+  } catch (err) {
+    // ignore
+  }
 }
